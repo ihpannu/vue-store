4fd5114c1c3c5f1adc74804b36808de14ff3dbd5
Harman Pannu <iharmanpannu@gmail.com>
2018-04-08 11:24:52 -0700

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: `Hello World`,
   props: {
     msg: String
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld />
+    <Cart/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import Cart from './Cart';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Cart
   }
 };
 </script>
